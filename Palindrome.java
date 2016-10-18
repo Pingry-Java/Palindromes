9e75ad68d3f6e7ec44e0e5c4068d8fdbf8a14475
Josh Orndorff <jorndorff@pingry.org>
2016-10-18 12:28:06 -0400

    Initial Commit. Starter code (hopefully) complete.

---
+++ Palindrome.java
@@ -0,0 +1,61 @@
+
+/**
+ * This class has four methods that each test whether the given string is a
+ * Palindrome. The four methods illustrate four different ways to test for
+ * palindromes
+ *
+ * @author AP Computer Science Class 2016
+ * @version 0.1
+ */
+ 
+public class Palindrome {
+
+  public static void main(String args[]) {
+    String testString = "banana";
+    System.out.println(iterativeCharAt(testString));
+    System.out.println(recursiveCharAt(testString));
+    System.out.println(iterativeSubstring(testString));
+    System.out.println(recursiveSubstring(testString));
+  }
+
+ /**
+  *   This method tests for palindrome-ness using a loop and the charAt method.
+  *   @param s the String to check whether it is a palindrome
+  *   @return boolean true if palindrome, false otherwise
+  */
+  public static boolean iterativeCharAt(String s)
+  {
+    //TODO Implement this.
+  }
+  
+ /**
+  *   This method tests for palindrome-ness using recursion and the charAt method.
+  *   @param s the String to check whether it is a palindrome
+  *   @return boolean true if palindrome, false otherwise
+  */
+  public static boolean recursiveCharAt(String s)
+  {
+    //TODO Implement this.
+  }
+  
+ /**
+  *   This method tests for palindrome-ness using a loop and the substring method.
+  *   @param s the String to check whether it is a palindrome
+  *   @return boolean true if palindrome, false otherwise
+  */
+  public static boolean iterativeSubstring(String s)
+  {
+    //TODO Implement this.
+  }
+  
+ /**
+  *   This method tests for palindrome-ness using recursion and the substring method.
+  *   @param s the String to check whether it is a palindrome
+  *   @return boolean true if palindrome, false otherwise
+  */
+  public static boolean recursiveSubstring(String s)
+  {
+    //TODO Implement this.
+  }
+
+}
